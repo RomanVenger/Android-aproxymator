613e7dce96b28e0b2e409343f3ab1c62c0e4cdb0
RomanVenger <33270394+RomanVenger@users.noreply.github.com>
2018-02-16 23:39:56 +0200

    Add files via upload

---
+++ Imnk.java
@@ -0,0 +1,18 @@
+package aprox_mnk;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by romanvenger on 11.02.2018.
+ */
+
+public interface Imnk {
+   public void makeArr();
+   public   double[][] createMartix(double[][]Arr);
+
+
+
+
+
+}
