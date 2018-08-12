9d2046e8d63058a2dabb06cb118cac1999d67fdc
RomanVenger <33270394+RomanVenger@users.noreply.github.com>
2018-08-12 21:58:29 +0300

    Update and rename Mnk.java to Offer.java

---
+++ Offer.java
@@ -0,0 +1,95 @@
+package Parse;
+
+import java.util.List;
+
+public class Offer {
+    private String name , brand, colour, description, price, initialPrice, articleID, shippingCosts;
+    private List<String> colors;
+
+    public Offer(){
+
+    }
+
+    public Offer(String brand, String colour, String description, String price, String initialPrice, String articleID, String shippingCosts, List<String> colors) {
+        this.brand = brand;
+        this.colour = colour;
+        this.description = description;
+        this.price = price;
+        this.initialPrice = initialPrice;
+        this.articleID = articleID;
+        this.shippingCosts = shippingCosts;
+        this.colors = colors;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getInitialPrice() {
+        return initialPrice;
+    }
+
+    public void setInitialPrice(String initialPrice) {
+        this.initialPrice = initialPrice;
+    }
+
+    public String getArticleID() {
+        return articleID;
+    }
+
+    public void setArticleID(String articleID) {
+        this.articleID = articleID;
+    }
+
+    public String getShippingCosts() {
+        return shippingCosts;
+    }
+
+    public void setShippingCosts(String shippingCosts) {
+        this.shippingCosts = shippingCosts;
+    }
+
+    public List<String> getColors() {
+        return colors;
+    }
+
+    public void setColors(List<String> colors) {
+        this.colors = colors;
+    }
+}
